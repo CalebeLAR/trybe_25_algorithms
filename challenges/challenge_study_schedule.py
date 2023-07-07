51fb3b356bafc42be0d00290e4dc812b52c94ea6
calebeLAR <ca6ele@gmail.com>
2023-07-07 19:49:51 -0400

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,39 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    permanence_period: uma lista de tuplas (a, b) onde a >= b com a, b inteiros
+    positivos
+    target_time: inteiro positivo
+
+    Retorna a quantidade de estudantes presentes em um target_time especifico
+    ou Node caso resebe um valor invalido
+
+    Um estudante só estava permanente em um dado periodo target_time se:
+    1) O target_time é igual ao horário de entrada do estudante
+    2) O target_time é igual ao horário de saída do estudante
+    3) O target_time é menor que o horário de entrada do estudante e
+    maior que o horário de saida do estudante
+    """
+    number_of_students_in_period = 0
+
+    if not isinstance(target_time, int):
+        return None
+
+    for entry, exit in permanence_period:
+
+        are_integers = isinstance(entry, int) and isinstance(exit, int)
+
+        if not are_integers or (exit < entry) or entry < 0 or exit < 0:
+            return None
+
+        p1 = (entry == target_time) or (exit == target_time)
+        p2 = (entry < target_time) and (exit > target_time)
+
+        if p1 or p2:
+            number_of_students_in_period += 1
+
+    return number_of_students_in_period
+
+
+# permanence_period = [(2, 5), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# target_time = 5
+# print(study_schedule(permanence_period, target_time))
