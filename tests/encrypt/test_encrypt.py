4220228b61601732c65b23ec7b724c266745a3a9
calebeLAR <ca6ele@gmail.com>
2023-07-11 00:21:01 -0400

    requisito 02: tentativa

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # O teste rejeita implementações que invertem a lógica de "par ou ímpar";
+    assert encrypt_message("arogauossap", 5) == "agora_passou"
+
+    # O teste rejeita implementações que não aplicam a regra de índice
+    # positivo válido;
+    assert encrypt_message("0123456789", -1) == "9876543210"
+
+    # O teste rejeita implementações que aplicam ordenação ao invés de inversão
+    assert encrypt_message("0123456789", 10) == "9876543210"
+
+    # O teste rejeita implementações que não validam o tipo das entradas;
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message_valida", "3")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(False, 3)
+
+    # O teste aprova implementações corretas.
+    assert encrypt_message("arogauossap", 5) == "agora_passou"
+    assert encrypt_message("odnassapaunitnoc", 8) == "continua_passando"
