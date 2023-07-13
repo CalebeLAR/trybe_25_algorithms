10b75955f45ea1ad1bd8e86cb422a6ff2efb7926
calebeLAR <ca6ele@gmail.com>
2023-07-13 12:49:46 -0400

    requisito 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,25 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    1) pego a posição da primeira e da ultima letra da palavra
+    2) se as letras forem iguais e estiverem em posições diferentes eu sigo
+    3) subtraio a primeia e a ultima letra da palavra
+    4) se o resultado dessa subtração não for uma string vazia eu sigo
+    5) repito esse processo até um deles falhar ou a subtração das letras
+    resultar uma string vazia
+
+    retira dos extremos, verifica a igualdade e repete
+    """
+    word = word[low_index:high_index + 1]
+
+    f = len(word) - 1
+
+    if word == "":
+        return False
+
+    if (word[0] == word[f]) is False and 0 != f:
+        return False
+
+    if word[1:f] == "":
+        return True
+
+    return is_palindrome_recursive(word[1:f], 0, f)
