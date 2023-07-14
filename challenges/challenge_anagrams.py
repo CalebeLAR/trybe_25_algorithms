0799535bf5c695f49cd1d822b1b26f47c1cd6dbb
calebeLAR <ca6ele@gmail.com>
2023-07-14 12:07:53 -0400

    requisito 04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,181 @@
+map_letters = {
+    "a": 0,
+    "A": 0,
+    "b": 1,
+    "B": 1,
+    "c": 2,
+    "C": 2,
+    "d": 3,
+    "D": 3,
+    "e": 4,
+    "E": 4,
+    "f": 5,
+    "F": 5,
+    "g": 6,
+    "G": 6,
+    "h": 7,
+    "H": 7,
+    "i": 8,
+    "I": 8,
+    "j": 9,
+    "J": 9,
+    "k": 10,
+    "K": 10,
+    "l": 11,
+    "L": 11,
+    "m": 12,
+    "M": 12,
+    "n": 13,
+    "N": 13,
+    "o": 14,
+    "O": 14,
+    "p": 15,
+    "P": 15,
+    "q": 16,
+    "Q": 16,
+    "r": 17,
+    "R": 17,
+    "s": 18,
+    "S": 18,
+    "t": 19,
+    "T": 19,
+    "u": 20,
+    "U": 20,
+    "v": 21,
+    "V": 21,
+    "w": 22,
+    "W": 22,
+    "x": 23,
+    "X": 23,
+    "y": 24,
+    "Y": 24,
+    "z": 25,
+    "Z": 25,
+}
+
+
+map_low_letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
+def case_firt(first_string, second_string):
+    second_maped = [map_letters[letter] for letter in second_string]
+    merge_sort(second_maped)
+
+    sorted_second_string = ""
+    for number in second_maped:
+        sorted_second_string += map_low_letters[number]
+
+    return (
+        first_string,
+        sorted_second_string,
+        False,
+    )
+
+
+def case_second(first_string, second_string):
+    first_maped = [map_letters[letter] for letter in first_string]
+    merge_sort(first_maped)
+
+    sorted_first_string = ""
+    for number in first_maped:
+        sorted_first_string += map_low_letters[number]
+
+    return (
+        sorted_first_string,
+        second_string,
+        False,
+    )
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Para esse problema podemos usar o algoritimo de ordenação merge sort
+    criando por Von Neumann.
+    Esse algorítimo, porém ordena listas de numeros, e para aproveita-lo nesse
+    problema em questão eu transformo as duas string a qual eu quero ordernar
+    em duas listas de numeros, onde cada numero representa a posição de sua
+    respectiva letra no alfabeto.
+    """
+    if first_string == "" or first_string == " ":
+        return case_firt(first_string, second_string)
+
+    if second_string == "" or second_string == " ":
+        return case_second(first_string, second_string)
+
+    first_maped = [map_letters[letter] for letter in first_string]
+    second_maped = [map_letters[letter] for letter in second_string]
+
+    merge_sort(first_maped)
+    merge_sort(second_maped)
+
+    sorted_first_string = ""
+    for number in first_maped:
+        sorted_first_string += map_low_letters[number]
+
+    sorted_second_string = ""
+    for number in second_maped:
+        sorted_second_string += map_low_letters[number]
+
+    return (
+        sorted_first_string,
+        sorted_second_string,
+        sorted_first_string == sorted_second_string,
+    )
